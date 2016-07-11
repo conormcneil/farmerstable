57f18865ec03d0cad5b08a3fceebed6c0def728e
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-07-10 18:59:17 -0600

    added search for markets

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,19 @@ app.controller('farmersTableController',['$scope','$http',function($scope,$http)
   $scope.view = {};
 
   $scope.view.getMarkets = function(zip) {
-    console.log(zip);
+    var url = 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=' + zip;
+    $http.get(url).then(function(data) {
+      $scope.view.markets = data.data.results;
+    });
+  };
+  $scope.view.moreInfo = function(id) {
+    $scope.view.marketDetails = null;
+    $scope.view.activeMarketId === id ? $scope.view.activeMarketId = null : $scope.view.activeMarketId = id;
+    var url = 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=' + id;
+    $http.get(url).then(function(data) {
+      $scope.view.marketDetails = data.data.marketdetails;
+      console.log(data.data.marketdetails);
+    });
   };
 
 }]);
