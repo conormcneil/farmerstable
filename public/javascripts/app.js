e0991f41cbcdeb746cb7ae7cf0a1aace92ddc802
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-08-10 12:30:24 -0600

    ngRoute

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,15 @@
-var app = angular.module('farmersTableApp',[]);
+var app = angular.module('farmersTableApp',['ngRoute']);
+
+app.config(["$routeProvider","$locationProvider",function($routeProvider,$locationProvider) {
+  $routeProvider
+    .when('/farmers', {
+      templateUrl: 'partials/farmers.html'
+    });
+    // .when('/resume', {
+    //   templateUrl: 'partials/resume.html'
+    // })
+    // .otherwise({
+    //   redirectTo: '/'
+    // });
+  $locationProvider.html5Mode(true);
+}]);
