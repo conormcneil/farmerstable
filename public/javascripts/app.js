bb24a93fb69582fac68c7bd563d4931c45026212
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-08-12 14:17:03 -0600

    still testing api

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -26,14 +26,14 @@ app.config(["$routeProvider","$locationProvider","$httpProvider",function($route
 }])
 .service('jwtInterceptor',function jwtInterceptor() {
   return {
-    request: function(config) {
-      var addJWT = (config.url.indexOf('http://localhost:3000') > -1);
-      var addHerokuJWT = (config.url.indexOf('https://farmerstable.herokuapp.com') > -1);
-      if (addHerokuJWT && localStorage.jwt || addJWT && localStorage.jwt) {
-        console.log('Adding Header', config.url);
-        config.headers.Authorization = 'Bearer ' + localStorage.jwt;
-      }
-      return config;
+    // request: function(config) {
+    //   var addJWT = (config.url.indexOf('http://localhost:3000') > -1);
+    //   var addHerokuJWT = (config.url.indexOf('https://farmerstable.herokuapp.com') > -1);
+    //   if (addHerokuJWT && localStorage.jwt || addJWT && localStorage.jwt) {
+    //     console.log('Adding Header', config.url);
+    //     config.headers.Authorization = 'Bearer ' + localStorage.jwt;
+    //   }
+    //   return config;
     }
   }
 })
