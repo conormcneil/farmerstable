efb05f867a648953ac51ac29e2e141f96e35c879
Conor Kingston <cmkingston21@me.com>
2016-08-24 13:50:15 -0600

    tests failing

--- protractor-tests/functional/config.js
+++
@@ -1,5 +0,0 @@
-exports.config = {
-  // make sure to start local selenium server: terminal $webdriver-manager start
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['index-spec.js']
-};
--- tests/functional/index.js
+++ tests/functional/index.js
@@ -1,31 +1,33 @@
 define(function (require) {
-    var registerSuite = require('intern!object');
-    var assert = require('intern/chai!assert');
+  var registerSuite = require('intern!object');
+  var assert = require('intern/chai!assert');
 
-    registerSuite({
-        name: 'log in',
+  registerSuite({
+    name: 'log in',
 
-        'log in form': function () {
-            return this.remote
-                .get(require.toUrl('http://localhost:3000/'))
-                .setFindTimeout(5000)
-                .findById('username')
-                    .click()
-                    .type('conor')
-                    .end()
-                .findById('password')
-                    .click()
-                    .type('conor')
-                    .end()
-                .findById('submit')
-                    .click()
-                    .end()
-                .findById('loggedinusername')
-                .getVisibleText()
-                .then(function (text) {
-                    assert.strictEqual(text, 'conor!',
-                        'username should be displayed when the user is logged in');
-                });
-        }
-    });
+    'users log in': function() {
+      return this.remote
+        .get(require.toUrl('http://localhost:3000/'))
+        .setFindTimeout(5000)
+        .findById('signin-button')
+          .click()
+          .end()
+        .findById('username')
+          .click()
+          .type('conor')
+          .end()
+        .findById('password')
+          .click()
+          .type('conor')
+          .end()
+        .findById('submit')
+          .click()
+          .end()
+        .findByModel('view.user.username')
+          .getVisibleText()
+          .then(function(text) {
+            assert.strictEqual(text, 'conor', 'user should be logged in');
+          });
+    }
+  });
 });
