f1d21f5e6fecce2d95c247ecff1bf8803b01d173
Conor Kingston <cmkingston21@me.com>
2016-09-06 15:02:17 -0600

    styled home page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('farmersTableApp',['ngRoute']);
+var app = angular.module('farmersTableApp',['ngRoute','ngLoadScript']);
 
 app.config(["$routeProvider","$locationProvider","$httpProvider",function($routeProvider,$locationProvider,$httpProvider) {
   $httpProvider.interceptors.push('jwtInterceptor');
@@ -44,4 +44,39 @@ app.config(["$routeProvider","$locationProvider","$httpProvider",function($route
       return config;
     }
   }
-})
+});
+
+/*global angular */
+(function (ng) {
+  'use strict';
+
+  var app = ng.module('ngLoadScript', []);
+
+  app.directive('script', function() {
+    return {
+      restrict: 'E',
+      scope: false,
+      link: function(scope, elem, attr)
+      {
+        if (attr.type==='text/javascript-lazy')
+        {
+          var s = document.createElement("script");
+          s.type = "text/javascript";
+          var src = elem.attr('src');
+          if(src!==undefined)
+          {
+              s.src = src;
+          }
+          else
+          {
+              var code = elem.text();
+              s.text = code;
+          }
+          document.head.appendChild(s);
+          elem.remove();
+        }
+      }
+    };
+  });
+
+}(angular));
