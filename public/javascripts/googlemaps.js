19bb549b40a168b9b9ee7d586576139ee41a9e50
Conor Kingston <cmkingston21@me.com>
2016-09-18 12:22:09 -0600

    responsive try catch

--- public/javascripts/googlemaps.js
+++ public/javascripts/googlemaps.js
@@ -1,14 +1,21 @@
-if (JSON.parse(localStorage.getItem('coords'))) {
-  var currentLocation = JSON.parse(localStorage.getItem('coords'));
-  console.log('coords exist',currentLocation);
-} else {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var currentLocation = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
-    console.log('new coords',currentLocation);
-    localStorage.setItem('coords',JSON.stringify(currentLocation))
-  });
+try {
+  if (JSON.parse(localStorage.getItem('coords'))) {
+    var currentLocation = JSON.parse(localStorage.coords);
+    console.log('coords exist',currentLocation);
+  } else {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var currentLocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      console.log('new coords',currentLocation);
+      localStorage.coords = JSON.stringify(currentLocation);
+    });
+  }
+} catch (e) {
+  var currentLocation = {
+    lat: 35,
+    lng: -110
+  }
 }
 // TODO: set expire time (~3-5min) to force coords update
